dcf8a46f37e93fdfb39cad37e33aa21258ab4e4d
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-27 09:11:35 +0530

    RefreshToken and logout

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var sock = undefined
 let qrRetry = 0
 
 const users = DataStore.create('Users.db')
+const userRefreshTokens = DataStore.create('UserRefreshTokens')
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -65,17 +67,71 @@ app.post('/api/auth/login', async (req, res) => {
         if (!passwordMatch) return res.status(401).json({ message: 'Email or password is incorrect'})
 
         const accessToken = jwt.sign({ userId: user._id , phoneNumber: user.phoneNumber}, config.accessTokenSecret, { subject:"accessApi", expiresIn:"1d"})
+        const refreshToken = jwt.sign({ userId: user._id , phoneNumber: user.phoneNumber}, config.refreshTokenSecret, { subject:"refreshToken", expiresIn:"1w"})
+
+        await userRefreshTokens.insert({
+            refreshToken,
+            userId: user._id
+        })
 
         return res.status(200).json({
             id: user._id,
             phoneNumber: user.phoneNumber,
-            accessToken
+            accessToken,
+            refreshToken
         })
     } catch(error) {
         return res.status(500).json({ message: error.message})
     }
 })
 
+app.get("api/auth/logout", authenticated, async (req, res) => {
+    try {
+        await userRefreshTokens.removeMany({ userId: req.user.id})
+        await userRefreshTokens.compactDataFile()
+
+        return res.status(204).send({ message: "Logged Out"})
+
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+})
+
+app.post("/api/auth/refresh-token", async (req,res) => {
+    try {
+        const { refreshToken } = req.body
+
+        if (!refreshToken) {
+            return res.status(401).json({ message: "Refresh Token is not found"})
+        }
+
+        const decodedRefreshToken = jwt.verify(refreshToken, config.refreshTokenSecret)
+
+        const userRefreshToken = await userRefreshTokens.findOne({ refreshToken, userId: decodedRefreshToken.userId})
+        if(!userRefreshToken) return res.status(401).json({ message: 'Refresh Token invalid or expired'})
+
+        await userRefreshTokens.remove({ _id: userRefreshToken._id})
+        await userRefreshTokens.compactDataFile()
+
+        const accessToken = jwt.sign({ userId:  decodedRefreshToken.userId , phoneNumber:  decodedRefreshToken.phoneNumber}, config.accessTokenSecret, { subject:"accessApi", expiresIn:"1d"})
+        const newRefreshToken = jwt.sign({ userId:  decodedRefreshToken.userId , phoneNumber:  decodedRefreshToken.phoneNumber}, config.refreshTokenSecret, { subject:"refreshToken", expiresIn:"1w"})
+
+        await userRefreshTokens.insert({
+            refreshToken: newRefreshToken,
+            userId: decodedRefreshToken.userId
+        })
+
+        return res.status(200).json({
+            accessToken,
+            refreshToken: newRefreshToken
+        })
+
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError || error instanceof jwt.JsonWebTokenError) return res.status(401).json({message: "Refresh Token invalid or expired"})
+
+        res.status(500).json({message: error.message})
+    }
+})
 app.get('/api/user/me', authenticated, async (req, res) => {
     try {
         const user = await users.findOne({ _id: req.user.id})
@@ -98,12 +154,19 @@ async function authenticated(req, res, next) {
     if(!accessToken) return res.status(401).json({message: "Access token not found"})
     try {
         const decodedAccessToken = jwt.verify(accessToken, config.accessTokenSecret)
+
+        req.accessToken = { value: accessToken, exp: decodedAccessToken.exp}
         req.user = { id: decodedAccessToken.userId }
-        console.log(req.user)
 
         next()
     } catch (error) {
-        return res.status(401).json({ message: "Access Token is invalid"})
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: "Access Token expired", code: "AccessTokenExpired"})
+        } else if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ message: "Access Token invalid", code: "AccessTokenInvalid"})
+        } else {
+            return res.status(500).json({ message: error.message})
+        }
     }
 }
 
